6f0fd20d231a6e928f5b8f3d29ba5f3ce585423f
Vladimir Dmitriev <defurator@mail.ru>
2015-08-19 23:43:57 +0300

    header

--- index.php
+++ index.php
@@ -22,74 +22,101 @@
 </head>
 <body>
   <header class="header">
-    <nav class="topmenu container">
-      <ul class="topmenu_list">
-        <li class="topmenu_item">
-          <a href="" class="topmenu_link topmenu_link__active">Главная</a>
-        </li>
-        <li class="topmenu_item">
-          <a href="" class="topmenu_link">О компании</a>
-        </li>
-        <li class="topmenu_item">
-          <a href="" class="topmenu_link">Наши услуги</a>
-          <div class="topmenu_wrap">
-            <ul class="topmenu_list__l2">
-              <li class="topmenu_item__l2">
-                <a href="" class="topmenu_link__l2">Восстановление учёта</a>
-              </li>
-              <li class="topmenu_item__l2">
-                <a href="" class="topmenu_link__l2">Постановка учёта</a>
-              </li>
-              <li class="topmenu_item__l2">
-                <a href="" class="topmenu_link__l2">Полное ведение учёта</a>
-              </li>
-              <li class="topmenu_item__l2">
-                <a href="" class="topmenu_link__l2">Составление отчётности</a>
-              </li>
-              <li class="topmenu_item__l2">
-                <a href="" class="topmenu_link__l2">Консультирование</a>
-              </li>
-              <li class="topmenu_item__l2">
-                <a href="" class="topmenu_link__l2">Подготовка документов</a>
-              </li>
-              <li class="topmenu_item__l2">
-                <a href="" class="topmenu_link__l2">Доступ к программе 1С</a>
-              </li>
-              <li class="topmenu_item__l2">
-                <a href="" class="topmenu_link__l2">Документы для кредитов</a>
-              </li>
-              <li class="topmenu_item__l2">
-                <a href="" class="topmenu_link__l2">Налоговая оптимизация</a>
-              </li>
-              <li class="topmenu_item__l2">
-                <a href="" class="topmenu_link__l2">Учёт оплаты труда</a>
-              </li>
-              <li class="topmenu_item__l2">
-                <a href="" class="topmenu_link__l2">Персонифицированный учёт</a>
-              </li>
-              <li class="topmenu_item__l2">
-                <a href="" class="topmenu_link__l2">Годовая отчётность</a>
-              </li>
-            </ul>
-          </div>
-        </li>
-        <li class="topmenu_item">
-          <a href="" class="topmenu_link">Цены</a>
-        </li>
-        <li class="topmenu_item">
-          <a href="" class="topmenu_link">Гарантии</a>
-        </li>
-        <li class="topmenu_item">
-          <a href="" class="topmenu_link">Тарифы</a>
-        </li>
-        <li class="topmenu_item">
-          <a href="" class="topmenu_link">Полезная информация</a>
-        </li>
-        <li class="topmenu_item">
-          <a href="" class="topmenu_link">Контакты</a>
-        </li>
-      </ul>
+    <nav class="topmenu">
+      <div class="topmenu_inner container">
+        <ul class="topmenu_list">
+          <li class="topmenu_item">
+            <a href="" class="topmenu_link topmenu_link__active">Главная</a>
+          </li>
+          <li class="topmenu_item">
+            <a href="" class="topmenu_link">О компании</a>
+          </li>
+          <li class="topmenu_item">
+            <a href="" class="topmenu_link">Наши услуги</a>
+            <div class="topmenu_wrap">
+              <ul class="topmenu_list__l2">
+                <li class="topmenu_item__l2">
+                  <a href="" class="topmenu_link__l2">Восстановление учёта</a>
+                </li>
+                <li class="topmenu_item__l2">
+                  <a href="" class="topmenu_link__l2">Постановка учёта</a>
+                </li>
+                <li class="topmenu_item__l2">
+                  <a href="" class="topmenu_link__l2">Полное ведение учёта</a>
+                </li>
+                <li class="topmenu_item__l2">
+                  <a href="" class="topmenu_link__l2">Составление отчётности</a>
+                </li>
+                <li class="topmenu_item__l2">
+                  <a href="" class="topmenu_link__l2">Консультирование</a>
+                </li>
+                <li class="topmenu_item__l2">
+                  <a href="" class="topmenu_link__l2">Подготовка документов</a>
+                </li>
+                <li class="topmenu_item__l2">
+                  <a href="" class="topmenu_link__l2">Доступ к программе 1С</a>
+                </li>
+                <li class="topmenu_item__l2">
+                  <a href="" class="topmenu_link__l2">Документы для кредитов</a>
+                </li>
+                <li class="topmenu_item__l2">
+                  <a href="" class="topmenu_link__l2">Налоговая оптимизация</a>
+                </li>
+                <li class="topmenu_item__l2">
+                  <a href="" class="topmenu_link__l2">Учёт оплаты труда</a>
+                </li>
+                <li class="topmenu_item__l2">
+                  <a href="" class="topmenu_link__l2">Персонифицированный учёт</a>
+                </li>
+                <li class="topmenu_item__l2">
+                  <a href="" class="topmenu_link__l2">Годовая отчётность</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="topmenu_item">
+            <a href="" class="topmenu_link">Цены</a>
+          </li>
+          <li class="topmenu_item">
+            <a href="" class="topmenu_link">Гарантии</a>
+          </li>
+          <li class="topmenu_item">
+            <a href="" class="topmenu_link">Тарифы</a>
+          </li>
+          <li class="topmenu_item">
+            <a href="" class="topmenu_link">Полезная информация</a>
+          </li>
+          <li class="topmenu_item">
+            <a href="" class="topmenu_link">Контакты</a>
+          </li>
+        </ul>
+      </div>
     </nav>
+    <div class="header_bottom container">
+      <div class="header_section">
+        <div class="logo">
+          <a class="logo_link" href="/">Бухгалтерский успех</a>
+          <span class="logo_text">Бухгалтерские услуги №1 в Беларуси</span>
+        </div>
+      </div>
+      <div class="header_section">
+        <div class="address">
+          <div class="address_left">
+            <p>+375 <span>(17)</span> 385 94 80</p>
+            <p>+375 (29) 385 94 80</p>
+            <p>+375 (29) 335 94 80</p>
+          </div>
+          <div class="address_right">
+            <p>ул. Притыцкого, 83</p>
+            <p>пом. 33н, каб. 2</p>
+            <p>Минск, 220140, Беларусь</p>
+          </div>
+        </div>
+      </div>
+      <div class="header_section">
+        <a class="button button__phone" href="/">Заказать звонок</a>
+      </div>
+    </div>
   </header>
 </body>
 </html>
