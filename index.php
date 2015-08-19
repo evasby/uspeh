5ff3e9da336bfea44fd63c10657de3f29b817d77
Vladimir Dmitriev <defurator@mail.ru>
2015-08-19 12:55:17 +0300

    topmenu

--- index.php
+++ index.php
@@ -21,6 +21,75 @@
   </script>
 </head>
 <body>
-  
+  <header class="header">
+    <nav class="topmenu container">
+      <ul class="topmenu_list">
+        <li class="topmenu_item">
+          <a href="" class="topmenu_link topmenu_link__active">Главная</a>
+        </li>
+        <li class="topmenu_item">
+          <a href="" class="topmenu_link">О компании</a>
+        </li>
+        <li class="topmenu_item">
+          <a href="" class="topmenu_link">Наши услуги</a>
+          <div class="topmenu_wrap">
+            <ul class="topmenu_list__l2">
+              <li class="topmenu_item__l2">
+                <a href="" class="topmenu_link__l2">Восстановление учёта</a>
+              </li>
+              <li class="topmenu_item__l2">
+                <a href="" class="topmenu_link__l2">Постановка учёта</a>
+              </li>
+              <li class="topmenu_item__l2">
+                <a href="" class="topmenu_link__l2">Полное ведение учёта</a>
+              </li>
+              <li class="topmenu_item__l2">
+                <a href="" class="topmenu_link__l2">Составление отчётности</a>
+              </li>
+              <li class="topmenu_item__l2">
+                <a href="" class="topmenu_link__l2">Консультирование</a>
+              </li>
+              <li class="topmenu_item__l2">
+                <a href="" class="topmenu_link__l2">Подготовка документов</a>
+              </li>
+              <li class="topmenu_item__l2">
+                <a href="" class="topmenu_link__l2">Доступ к программе 1С</a>
+              </li>
+              <li class="topmenu_item__l2">
+                <a href="" class="topmenu_link__l2">Документы для кредитов</a>
+              </li>
+              <li class="topmenu_item__l2">
+                <a href="" class="topmenu_link__l2">Налоговая оптимизация</a>
+              </li>
+              <li class="topmenu_item__l2">
+                <a href="" class="topmenu_link__l2">Учёт оплаты труда</a>
+              </li>
+              <li class="topmenu_item__l2">
+                <a href="" class="topmenu_link__l2">Персонифицированный учёт</a>
+              </li>
+              <li class="topmenu_item__l2">
+                <a href="" class="topmenu_link__l2">Годовая отчётность</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li class="topmenu_item">
+          <a href="" class="topmenu_link">Цены</a>
+        </li>
+        <li class="topmenu_item">
+          <a href="" class="topmenu_link">Гарантии</a>
+        </li>
+        <li class="topmenu_item">
+          <a href="" class="topmenu_link">Тарифы</a>
+        </li>
+        <li class="topmenu_item">
+          <a href="" class="topmenu_link">Полезная информация</a>
+        </li>
+        <li class="topmenu_item">
+          <a href="" class="topmenu_link">Контакты</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </body>
 </html>
