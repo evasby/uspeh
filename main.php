46919a95f0cb9cd3c1b94776cc9b49e4e1464125
Vladimir Dmitriev <defurator@mail.ru>
2015-08-25 12:49:23 +0300

    calc begin

--- main.php
+++ main.php
@@ -16,8 +16,9 @@
   <script src="//maps.googleapis.com/maps/api/js?v=3.exp"></script>
   <script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/src/infobox.js"></script>
   <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script type="text/javascript" src="js/jquery.placeholder.js"></script>
   <script src="js/script.js"></script>
-  <script src="js/gmap.js"></script>
+  <!--<script src="js/gmap.js"></script>-->
   
 </head>
 <body>
@@ -118,14 +119,102 @@
       </div>
     </div>
   </header>
-  <div class="title title__info">
+  <!--<div class="title title__info">
     <div class="title_wrap container">
       <h1 class="title_h1">Полезная информация</h1>
-      <p class="title_descr">Здесь можно подробно ознакомиться с полезными анонсами, новостями, новостями компании Бухгалтерский успех</p>
+      <div class="title_descr">
+        <p>Здесь можно подробно ознакомиться с полезными анонсами, новостями, новостями компании Бухгалтерский успех</p>
+      </div>
     </div>  
+  </div>-->
+  <div class="title title__main">
+    <div class="title_wrap container">
+      <h1 class="title_h1 title_h1__main">Аутсортинг бухгалтерских услуг в Минске</h1>
+      <div class="title_descr title_descr__main">
+        <p>Экономия до 30% средств</p>
+        <p>Штат из 25 бухгалтеров</p>
+      </div>
+      <div class="form-discount">
+        <div class="form-discount_transp"></div>
+        <div class="form-discount_wrap">
+          <form action="#">
+            <div class="form-discount_title">Получите скидку<span>15%</span></div>
+            <div class="form-discount_inner">
+              <input class="form-text" type="text" maxlength="128" size="60" value="" placeholder="Ваше имя">
+            </div>
+            <div class="form-discount_inner">
+              <input class="form-text" type="text" maxlength="128" size="60" value="" placeholder="Ваш телефон">
+            </div>
+            <div class="form-discount_button">
+              <input class="webform-submit button-primary form-submit ajax-processed" type="submit" value="Получить" name="op">
+            </div>
+            <div class="form-discount_descr">Введите ваше имя и телефон. Специалисты Бухгалтерский успех свяжутся с вами в течение 30 минут.</div>
+          </form>
+        </div>
+      </div>
+      <div class="main-descr">
+        <div class="main-descr_title">Акция!</div>
+        <div class="main-descr_descr">
+          Закажите бухгалтерские услуги до 10 июля и получите скидку в размере 15%
+        </div>
+      </div>
+    </div>
   </div>
+
+  <div class="arrow-bb"></div>
+
+  <div class="main1 container" id="main1">
+    <div class="main1_head">Важное о бухгалтерском успехе</div>
+    <div class="main1_wrap">
+      <div class="main1_item">
+        <div class="main1_image"><img src="image/down/main1-1.png" alt=""></div>
+        <div class="main1_title">Риски застрахованы</div>
+        <div class="main1_text">Подготовленная отчетность проверяется внутренним аудитором для избежания возможных недочетов</div>
+      </div>
+      <div class="main1_item">
+        <div class="main1_image"><img src="image/down/main1-2.png" alt=""></div>
+        <div class="main1_title">Высокая квалификация бухгалтеров</div>
+        <div class="main1_text">Система жесткого отбора специалистов при приеме на работу, участие сотрудников в семинарах внутри компании и профильных конференциях партнера</div>
+      </div>
+      <div class="main1_item">
+        <div class="main1_image"><img src="image/down/main1-3.png" alt=""></div>
+        <div class="main1_title">Внутренний контроль качества</div>
+        <div class="main1_text">Возможные убытки, связанные с недочетами в бухгалтерии, застрахованы на сумму до 500 млн. руб, что дает дополнительную уверенность клиентам</div>
+      </div>
+      <div class="main1_item">
+        <div class="main1_image"><img src="image/down/main1-4.png" alt=""></div>
+        <div class="main1_title">Экономия более 30%</div>
+        <div class="main1_text">Вы экономите на налогах, расходах на бухгалтерское и юридическое программное обеспечение, других офисных расходах, связаных с содержанием бухгалтера в штате</div>
+      </div>
+    </div>
+  </div>
+
+  <div class="main2" id="main2">
+    <div class="main2_head"><span>Калькулятор тарифа</span></div>
+    <div class="main2_wrap container">
+      <div class="main2_descr">Рассчитайте стоимость обслуживания под ваши нужды</div>
+      <div class="calc-tax">
+        <div class="calc-tax_head">Система налогооблажения</div>
+        <div class="calc-tax_wrap">
+          <div class="calc-tax_item">
+            <input id="rfirst" type="radio" name="radio" checked hidden />
+            <label for="rfirst">Checked radio</label>
+          </div>
+          <div class="calc-tax_item">
+            <input id="rsecond" type="radio" name="radio" hidden />
+            <label for="rsecond">Unchecked radio</label>
+          </div>
+          <div class="calc-tax_item">
+            <input id="rthird" type="radio" name="radio" hidden />
+            <label for="rthird">Disabled radio</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <footer class="footer">
-    <div class="gmap" id="gmap1"></div>
+    <div class="gmap" id="gmap"></div>
     <div class="footer_bottom container">
       <div class="footer_wrap">
         <div class="footer_section">
